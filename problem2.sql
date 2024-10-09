bdd34c9274d98960ae237a835e1e1e5f0d3ce09f
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-09 20:53:14 -0230

    Created tables

--- problem2.sql
+++ problem2.sql
@@ -5,12 +5,40 @@
 
 --Create Tables
 --Products
+CREATE TABLE products (
+    id serial NOT NULL PRIMARY KEY,
+    product_name varchar(100),
+    price bigint, --price in cents
+    stock_quantity int,
+    UNIQUE id
+);
 
 --Customers
+CREATE TABLE customers (
+    id serial NOT NULL PRIMARY KEY,
+    first_name varchar(100),
+    last_name varchar(100),
+    email varchar(200),
+    UNIQUE id
+);
 
 --Orders
+CREATE TABLE orders (
+    id serial NOT NULL PRIMARY KEY,
+    customer_id int,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(id)
+);
 
 --Order Items
+CREATE TABLE order_items (
+    order_id int,
+    product_id int,
+    quantity int,
+    FOREIGN KEY order_id REFERENCES orders(id),
+    FOREIGN KEY product_id REFERENCES products(id),
+    PRIMARY KEY (order_id, product_id)
+);
 
 --Insert Data
 
