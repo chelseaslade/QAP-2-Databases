8a6754c5f8c3dc2cb834a72c8ee6b79c934b5139
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-09 10:22:12 -0230

    Tables created

--- problem1.sql
+++ problem1.sql
@@ -6,18 +6,42 @@
 --Create Tables
 --Students
 CREATE TABLE students (
-    id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id int NOT NULL AUTO_INCREMENT,
     first_name varchar(100),
     last_name varchar(100),
     email varchar (150),
     school_enrollment_date DATE
+    PRIMARY KEY (id)
 );
 
 --Professors
+CREATE TABLE professors (
+    id int NOT NULL AUTO_INCREMENT,
+    first_name varchar(100),
+    last_name varchar(100),
+    department varchar(100)
+    PRIMARY KEY (id)
+);
 
 --Courses
+CREATE TABLE courses (
+    id int NOT NULL AUTO_INCREMENT,
+    course_name varchar(100),
+    course_description varchar(300),
+    professor_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (professor_id) REFERENCES professors(id)
+);
 
 --Enrollments
+CREATE TABLE enrollments (
+    student_id int,
+    course_id int,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (course_id) REFERENCES courses(id),
+    enrollment_date DATE,
+    PRIMARY KEY (student_id, course_id)
+);
 
 --Insert Data 
 
