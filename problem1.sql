f433047acf65075a64ba8b0aa629454f15600a86
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-10-09 10:01:20 -0230

    File init + basic layout of problems

---
+++ problem1.sql
@@ -0,0 +1,25 @@
+--QAP 2: SQL & PostgreSQL Mastery
+--Problem 1: University Course Enrollment System
+--Author: Chelsea Slade (Mayne)
+--Due: October 14, 2024
+
+--Create Tables
+--Students
+
+--Professors
+
+--Courses
+
+--Enrollments
+
+--Insert Data 
+
+--Retrieve full names of all students in Physics 101
+
+--Retrieve list of all courses + full prof name of respective teacher
+
+--Retrieve all courses that have students enrolled
+
+--Update Student Email
+
+--Remove a Student from Course
---
+++ problem2.sql
@@ -0,0 +1,26 @@
+--QAP 2: SQL & PostgreSQL Mastery
+--Problem 2: Online Store Inventory & Order System
+--Author: Chelsea Slade (Mayne)
+--Due: October 14, 2024
+
+--Create Tables
+--Products
+
+--Customers
+
+--Orders
+
+--Order Items
+
+--Insert Data
+
+--Retrieve names/stock quantities of all prod.
+
+--Retrieve product names/quantities of one placed order
+
+--Retrieve all orders placed by one customer (ID's & quantities)
+
+--Update to reduce stock quantity after customer order
+
+--Remove an order and associated order items from system
+
